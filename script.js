6afdfcd0645cdf91f73a9b9695e4236aabb4a7ab
Firat Yedibela <yedibelafirat0@gmail.com>
2023-12-29 02:33:22 +0300

    Implement displaying logic
    Update the display when number buttons or operator buttons are clicked

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let operand1;
 let operand2;
 let operator;
 
+const display = document.querySelector('#display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator')
+
+// Populate the display when clicked numbers
+numbers.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    display.textContent += event.target.textContent;
+  }); 
+});
+
+// Populate the display when clicked operators
+operators.forEach(operator => {
+  operator.addEventListener('click' , (event) => {
+    display.textContent += ' ' + event.target.textContent + ' ';
+  });
+});
+
 function add(a, b) {
   return a + b;
 }
